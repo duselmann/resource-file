c5b176ebe97c83303de182bcd229601c69be23ad
Bill Blondeau <wblondeau@usgs.gov>
2013-07-09 11:26:36 -0500

    Added StatusCode "label" field for what used to be "description"; repurposed "description" for verbose helpful explanations; added some more HTTP status codes.

--- src/gov/usgs/cida/servicefolder/StatusCode.java
+++ src/gov/usgs/cida/servicefolder/StatusCode.java
@@ -5,15 +5,32 @@ package gov.usgs.cida.servicefolder;
  * @author Bill Blondeau <wblondeau@usgs.gov>
  */
 public enum StatusCode {
-	OK_200(200, "OK"),
-	BAD_REQUEST_400(400, "Bad Request"),
-	SERVER_ERROR_500(500, "Internal Server Error");
+	OK_200(200, "OK", ""),
+	CREATED_201(201, "Created", "A new resource has been created. Its URI is "
+			+ "in the message body."),
+	ACCEPTED_202(202, "Accepted", "The request has been accepted for "
+			+ "processing, but is not yet done. A URI for tracking its status "
+			+ "is in the message body."),
+	BAD_REQUEST_400(400, "Bad Request", ""),
+	UNAUTHORIZED_401(401, "Unauthorized", "You must authenticate yourself "
+			+ "in order to use this service (assuming you have necessary "
+			+ "permissions in the first place.)"),
+	FORBIDDEN_403(403, "Forbidden", "If you have authenticated, your "
+			+ "permissions were insufficient. If you have not authenticated,"
+			+ "it wouldn't help: your access is being denied for a reason that "
+			+ "exceeds permissions."),
+	NOT_FOUND_404(404, "Not Found", ""),
+	NOT_FOUND_405(405, "Method Not Allowed", "Your request specified an "
+			+ "HTTP method (GET, POST, etc) that this service does not support."),
+	SERVER_ERROR_500(500, "Internal Server Error", "");
 	
 	private Integer code;
+	private String label;
 	private String description;
 	
-	private StatusCode (int code, String description){
+	private StatusCode (int code, String label, String description){
 		this.code = code;
+		this.label = label;
 		this.description = description;
 	}
 	
@@ -21,8 +38,16 @@ public enum StatusCode {
 		return this.code;
 	}
 	
+	public String getLabel() {
+		return this.label;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
 	@Override
 	public String toString() {
-		return (this.code + " " + this.description);
+		return (this.code + " " + this.label);
 	}
 }
